832d703040fb6c17ab7799957b81f3c750ffaf2e
Rayhanuc <kj\hf_yu9i[wehr>
2018-02-03 15:35:31 +0600

    Class-25
    dynamic some content of this theme.

--- functions.php
+++ functions.php
@@ -5,11 +5,18 @@ function themes_setup_func(){
 	add_theme_support( 'custom-header' );
 	add_theme_support( 'custom-background' );
 	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'title-tag' );
+
+	load_theme_textdomain( 'simpletheme', get_template_directory_uri().'/language' );
+	register_nav_menu( 'main-menu', __('Main Menu','simpletheme') );
+
+
+
 }
 
 add_action( 'wp_enqueue_scripts', function(){
 	wp_enqueue_style( 'anoceanofsky',get_template_directory_uri().'/css/anoceanofsky.css' );
-} )
+} );
 
 
 
--- header.php
+++ header.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html <?php language_attributes();?>>
 <head>
-<title>this is our first web site</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 <?php wp_head();?>
 </head>
 <body <?php body_class();?>>
-<div id="page">
-  <div class="topNaviagationLink"><a href="#">Home</a></div>
-  <div class="topNaviagationLink"><a href="#">About</a></div>
-  <div class="topNaviagationLink"><a href="#">Portfolio</a></div>
-  <div class="topNaviagationLink"><a href="#">Services</a></div>
-  <div class="topNaviagationLink"><a href="#">Contact</a></div>
-</div>
+
+
+
+<?php wp_nav_menu(array(
+	'theme_locale' => 'main_menu',
+	'menu_id' => 'page',
+	));
+?>
+
 <div id="mainPicture">
   <div class="picture" style="background-image: url('<?php echo header_image();?>');">
     <div id="headerTitle"><?php bloginfo('title');?></div>
